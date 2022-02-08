5d63378f7815e09d031d06e538792da685997529
Orizin <nlgporizin10@gmail.com>
2022-02-09 01:02:55 +0900

    スタート/ストップボタン切り替え実装/秒数カウント機能実装

---
+++ script.js
@@ -0,0 +1,32 @@
+// ディスプレイの秒数
+let seconds = 0
+// 1秒ごとにディスプレイに 1 追加するタイマー処理(setInterval) 用の変数
+let timer
+// ディスプレイ
+const display = document.getElementById('display')
+// スタート・ストップボタン
+const startStopButton = document.getElementById('start-stop')
+// リセットボタン
+const resetButton = document.getElementById('reset')
+
+// 「スタート」「ストップ」ボタンがクリックされたとき
+startStopButton.onclick = () => {
+  if (startStopButton.innerText === 'スタート') {
+    // 1秒(1000ミリ秒)ごとに addSecond 関数を呼び出す
+    setInterval(addSecond, 1000)
+
+    // 「スタート」ボタンを「ストップ」ボタンに変更
+    startStopButton.innerText = 'ストップ'
+  } else {
+    // 「ストップ」ボタンを「スタート」ボタンに変更
+    startStopButton.innerText = 'スタート'
+  }
+}
+
+// ディスプレイの秒数を1増やす
+const addSecond = () => {
+  // 秒数を1追加
+  ++seconds
+  // 秒数をディスプレイに反映
+  display.innerText = seconds
+}
